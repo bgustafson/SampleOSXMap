56835a150dc200ac7a74b71478418f511b06bdfe
Brian Gustafson <bmgustafson@Brians-Mac-mini.local>
2013-12-21 12:19:50 -0700

    Initial Commit

---
+++ SampleMapViewer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SampleMapViewer
+//
+//  Created by Brian Gustafson on 12/21/13.
+//  Copyright (c) 2013 Brian Gustafson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ SampleMapViewer/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SampleMapViewer
+//
+//  Created by Brian Gustafson on 12/21/13.
+//  Copyright (c) 2013 Brian Gustafson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
